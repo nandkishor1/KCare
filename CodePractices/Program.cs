79a4a951f61b00b35c4593ed175c9588209ef3be
Nandkishor Yadav <nandkishoryadav200695@gmail.com>
2021-11-08 14:27:51 +0530

    Hello

---
+++ CodePractices/Program.cs
@@ -0,0 +1,91 @@
+﻿//using System;
+
+//namespace DataTypes
+//{
+
+//    class MyDataType
+//    {
+//        public string show()
+//        {
+//            return ("MyType");
+//        }
+//    }
+//    class Program
+//    {
+
+
+//        static void Main(string[] args)
+//        {
+
+        
+
+//            Console.WriteLine("VALUE TYPE: ");
+//            Console.WriteLine("------------------------------------------------------------------------");
+//            Console.WriteLine("Integers:");
+//            byte a = 12;
+//            Console.WriteLine("byte\tA = {0}\t{1}\t{2}\t{3}", a, typeof(byte), byte.MaxValue, byte.MinValue);
+//            sbyte b = 12;
+//            Console.WriteLine("sbyte\tB = {0}\t{1}\t{2}\t{3}", b, typeof(sbyte), sbyte.MaxValue, sbyte.MinValue);
+//            short c = 12;
+//            Console.WriteLine("short\tC = {0}\t{1}\t{2}\t{3}", c, typeof(short), short.MaxValue, short.MinValue);
+//            ushort d = 12;
+//            Console.WriteLine("ushort\tD = {0}\t{1}\t{2}\t{3}", d, typeof(ushort), ushort.MaxValue, ushort.MinValue);
+//            int e = 12;
+//            Console.WriteLine("int\tE ={0}\t{1}\t{2}\t{3}", e, typeof(int), int.MaxValue, int.MinValue);
+//            uint f = 12;
+//            Console.WriteLine("uint\tF = {0}\t{1}\t{2}\t{3}", f, typeof(uint), uint.MaxValue, uint.MinValue);
+//            long g = 12;
+//            Console.WriteLine("long\tG = {0}\t{1}\t{2}\t{3}", g, typeof(long), long.MaxValue, long.MinValue);
+//            ulong h = 12;
+//            Console.WriteLine("ulong\tH = {0}\t{1}\t{2}\t{3}", h, typeof(ulong), ulong.MaxValue, ulong.MinValue);
+
+//            Console.WriteLine("\nFloating Point:");
+//            float i = 123.34f;
+//            Console.WriteLine("float\tI = {0}\t{1}\t{2}\t{3}", i, typeof(float), float.MaxValue, float.MinValue);
+//            double j = 123.34;
+//            Console.WriteLine("double\tJ = {0}\t{1}\t{2}\t{3}", j, typeof(double), double.MaxValue, float.MinValue);
+//            decimal k = 123.34m;
+//            Console.WriteLine("decimal\tM = {0}\t{1}\t{2}\t{3}", k, typeof(decimal), decimal.MaxValue, decimal.MinValue);
+
+//            Console.WriteLine("\nText:");
+//            char l = 'f';
+//            Console.WriteLine("char\tL = {0}\t{1}", l, typeof(char));
+//            Console.WriteLine("\nBoolean:");
+//            bool m = true;
+//            Console.WriteLine("bool\tM = {0}\t{1}", m, typeof(bool));
+
+//            Console.WriteLine("\nREFERENCE TYPE:");
+//            Console.WriteLine("..................................");
+//            string n = "Hello";
+//            Console.WriteLine("string\tN = {0}\t{1}",n, typeof(string));
+//            object o = 12.34;
+//            Console.WriteLine("Object\to = {0}\t{1}", o, typeof(object));
+
+//            MyDataType p = new MyDataType();
+//            Console.WriteLine("MyDataType\tp = {0}\t{1}", p.show(), typeof(MyDataType));
+//            int[] q = { 12, 45, 56 };
+
+//            Console.WriteLine("int[]\tq = {0}", typeof(int[]));
+
+//            Console.WriteLine("\nNullable Type:");
+//            Console.WriteLine(".........................................................");
+//            string r = null;
+//            Console.WriteLine("string\tr = {0}\t{1}", r, typeof(string));
+//            int? s = null;
+//            Console.WriteLine("int\ts = {0}\t{1}", s, typeof(int?));
+
+//            Console.ReadKey();
+
+
+
+
+
+
+
+
+        
+
+//        }
+//    }
+//}
+    
---
+++ CodePractices/SwappNumUseXOR.cs
@@ -0,0 +1,30 @@
+﻿//C# program to convert a binary number into a decimal number
+
+using System;
+namespace binaryIntoDecimal
+{
+    class Con_bin_dec
+    {
+        static void Main(string[] args)
+        {
+            int binNum = 0;
+            int decNum = 0;
+            int i = 0;
+            int rem = 0;
+
+            Console.Write("Enter a binary number: ");
+            binNum = int.Parse(Console.ReadLine());
+
+            while (binNum > 0)
+            {
+                rem = binNum % 10;
+                decNum = decNum + rem * (int)Math.Pow(2, i);
+                binNum = binNum / 10;
+                i++;
+
+            }
+            Console.WriteLine("\nDecimal number: " + decNum);
+
+        }
+    }
+}
